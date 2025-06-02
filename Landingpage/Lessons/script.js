5ac61e1de2117f457a5cc5354a2346d3daf12537
fyneboy001 <fyneboyfynerose@gmail.com>
2025-06-02 14:55:54 +0100

    Adding new modifications

---
+++ Landingpage/Lessons/script.js
@@ -0,0 +1,15 @@
+//Hamburger Menu Styling
+const hamburger = document.getElementById("hamburgerToggle");
+const sideNav = document.querySelector(".SideNav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  sideNav.classList.toggle("active");
+});
+
+document.querySelectorAll(".Nav-links").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    sideNav.classList.remove("active");
+  })
+);
